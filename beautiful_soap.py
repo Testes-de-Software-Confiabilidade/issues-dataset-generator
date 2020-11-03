3b958271f2addd979e7d4c0da204ec34f5c933b0
Durval Carvalho <durvalcsouza@outlook.com>
2020-11-03 09:49:09 -0300

    Add request script to get issue dates

--- beautiful_soap.py
+++ beautiful_soap.py
@@ -1,12 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 issues_ids = []
 
+def get_posix_date(id):
+	issue_page = requests.get('https://api.github.com/repos/vuejs/vue/issues/%s/events' % id)
+	created_at = issue_page.json()[0]['created_at']
+
+	created_at = created_at.split('T')
+	created_at = created_at[0] + ' ' + created_at[-1].replace('Z','')
+	created_at = datetime.strptime(created_at, "%Y-%m-%d %H:%M:%S")
+	posix_date = int((created_at - datetime(1970,1,1)).total_seconds())
+
+	return posix_date
+
+
 for i in range(1, 1000):
 	page = requests.get("https://github.com/vuejs/vue/issues?page=%s&q=label:bug+is:closed" % (i))
-
-	print('page ' + str(i) + ' status ' + page.headers['status'][:3])
 
 	soup = BeautifulSoup(page.content, 'html.parser')
 	matches = soup.findAll("div", {"class": "js-issue-row"})
@@ -14,7 +25,14 @@ for i in range(1, 1000):
 	if(matches == []):
 		break
 
-	issues_ids += [a.attrs['id'] for a in matches]
+	for issue in matches:
+		id = issue.attrs['id'].replace('issue_', '')
 
+		print(id, end=', ')
+		print(get_posix_date(id))
+		issues_ids += (id, get_posix_date(id))
+		# print(id, get_posix_date(id))
 
-print(issues_ids)
+# for issue_id, posix_date in issues_ids:
+# 	print(issue_id, posix_date)
+
