ad8a6db8a82c5d7db1a8ab95a03c39e0fcff2f7b
Durval Carvalho <durvalcsouza@outlook.com>
2020-11-05 12:45:49 -0300

    Add Issue and Repository abstraction

---
+++ IssueClass.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+class Issue:
+    def __init__(self, id, closed_at, created_at, state, url):
+        self._id = id.strip()
+        
+        self._closed_at = datetime.strptime(closed_at.strip(), '%Y-%m-%d %H:%M:%S')
+        self._created_at = datetime.strptime(created_at.strip(), '%Y-%m-%d %H:%M:%S')
+
+        self._state = state.strip()
+        self._url = url.strip()
+
+    def __str__(self):
+        return f'{self._id}, {self._closed_at}, {self._created_at}, {self._state}, {self._url}'
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def closed_at(self):
+        return self._closed_at
+
+    @property
+    def created_at(self):
+        return self._created_at
+
+    @property
+    def created_at(self):
+        return self._created_at
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def url(self):
+        return self._url
---
+++ RepositoryClass.py
@@ -0,0 +1,12 @@
+class Repository:
+    def __init__(self, url, filters_rules):
+        self.url = url
+
+        labels_rules = filters_rules['labels']
+        
+        self.must_have_labels = set(labels_rules['must_have'])
+        self.blocklist_labels = set(labels_rules['blocklist_labels'])
+
+        self.name = url.split('/')[1].replace('.', '')
+        self.dataset_file = 'datasets/' + self.name + '.csv'
+        self.chart_name = 'graph/' + self.name + '.png'
