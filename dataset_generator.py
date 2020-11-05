88c3b61ad15c2f10198850bbec3c8ab9ab8a8507
Durval Carvalho <durvalcsouza@outlook.com>
2020-11-05 12:51:20 -0300

    Modify script structure

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,40 +1,97 @@
+import github
+from github.PaginatedList import PaginatedList
+from RepositoryClass import Repository
+from IssueClass import Issue
+import datetime
+import time
+
 class DatasetGenerator:
-	def __init__(self, token, repo_name, issues_filters):
-		from github import Github
-		g = Github(token)
-		repo = g.get_repo(repo_name)
-		
-		issues_filters['state'] = 'closed'
-		self.closed_issues = list(repo.get_issues(**issues_filters))
-		issues_filters['state'] = 'open'
-		self.closed_issues += list(repo.get_issues(**issues_filters))
+    def __init__(self, token, repository, loadFromFile=False):
+        self.g = github.Github(token)
+        self.repository = repository
+        self.repository_connection = self.g.get_repo(repository.url)
 
-	def export_dataset(self, filename):
-		dates = []
+        if(loadFromFile):
+            self.filtered_issues = self.read_csv()
+            
+        else:
+            self.all_issues = self.repository_connection.get_issues(state='all')
+            self.filtered_issues = self.apply_filters()
 
-		years_counter = 0
-		month_prev = 0
 
-		for issue in self.closed_issues:
-			date = issue.created_at.replace(day=1, hour=0, minute=0, second=0)
-			print(date)
-			dates.append(date)
+    def read_csv(self):
+        filtered_issues = []
+        with open(self.repository.dataset_file, 'r') as file:
+            file.readline() # jump csv header line
 
-		self.months = []
+            for issue in file:
+                issue_data = issue.strip().split(',')
+                filtered_issues.append(Issue(*issue_data))
 
-		with open(filename, 'w') as file:
-			for date in sorted(dates):
-				month = date.month
+        return filtered_issues
 
-				if(month_prev > month):
-					years_counter += 1
 
-				month_prev = month
-				month = years_counter * 12 + month
+    def is_valid(self, issue):
+        all_labels = set()
 
-				file.write(str(month) + '\n')
+        for label in issue.labels:
+            all_labels.add(label.name)
+            if(label.name in self.repository.blocklist_labels):
+                return False
+            
+        for label in self.repository.must_have_labels:
+            if(label not in all_labels):
+                return False
 
-				self.months.append(month)
-		
-		return self.months
+        return True
+
+
+    def apply_filters(self):
+        filtered_issues = []
+        
+        with open(self.repository.dataset_file, 'w') as file:
+            file.write('"id", "closed_at", "created_at", "state", "url"\n')
+
+            for i, issue in enumerate(self.all_issues):
+                perc = f'{i / self.all_issues.totalCount:.2f}%'
+                print(i, self.all_issues.totalCount, perc,  self.g.get_rate_limit())
+
+                if(issue.pull_request):
+                    continue
+
+                if(self.is_valid(issue)):
+                    filtered_issues.append(issue)
+                    file.write((f"{issue.number}, {issue.closed_at}, {issue.created_at}, {issue.state}, {issue.url}\n"))
+
+                    print(f'{issue.number} {issue.html_url} is valid\n')
+                else:
+                    print(f'{issue.number} {issue.html_url} is invalid\n')
+
+
+        print('%s issues válidas' % len(filtered_issues))
+        return filtered_issues
+
+
+    def get_issue_months(self):
+        dates = []
+        for issue in self.filtered_issues:
+            date = issue.created_at.replace(day=1, hour=0, minute=0, second=0)
+            dates.append(date)
+
+        dates = sorted(dates)
+
+        months = []
+        prev_date = None
+        years_counter = 0
+
+        for date in dates:
+            if(prev_date):
+                years_counter += (date.year - prev_date.year)
+            
+            prev_date = date
+
+            month = years_counter * 12 + date.month
+            months.append(month)
+        
+        return months
 
